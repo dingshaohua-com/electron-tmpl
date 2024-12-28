812a07ec5f02a688fe4f44861b6b3141e309cc92
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-28 16:51:48 +0800

    fix: 增加打包

--- bin/build.js
+++ bin/build.js
@@ -1,25 +1,31 @@
 import fs from "node:fs";
 import path from "node:path";
-import { spawn, spawnSync } from "node:child_process";
 import fsExtra from "fs-extra/esm";
+import {spawn, spawnSync } from "node:child_process";
+
+
+const args = process.argv.slice(2);
+const projectName = args[0];
 
 const projectRoot = path.resolve(process.cwd());
 const rootDist = path.resolve(projectRoot, "dist");
 
-// 获取都有哪些项目
-const ignoreDir = [".DS_Store"];
-const appsDir = path.resolve("src", "apps");
-const apps = fs.readdirSync(appsDir, { withFileTypes: true }).filter(it=>!ignoreDir.includes(it.name));
+const webPath = path.resolve("src","apps","web");
+const electronPath = path.resolve("src","apps","electron");
 
-// 开始打包
-apps.forEach((app) => {
-  if (ignoreDir.includes(app.name)) return false;
-  const appPath = path.resolve(app.parentPath, app.name);
-  spawnSync("npm run", ["--prefix", appPath, "build"], {
-    stdio: "inherit",
-    shell: true,
-  });
-  const appDist = path.resolve(appPath, "dist");
-  const appDistNewPath = path.resolve(rootDist, app.name);
-  fsExtra.moveSync(appDist, appDistNewPath, { overwrite: true });
-});
+fs.rmSync("dist", { recursive: true, force: true });
+
+if(projectName === "electron"){
+    const electronRenderder = path.resolve(electronPath, "src", "renderder");
+    const webDist = path.resolve(webPath, "dist");
+    // web端打包并移动到electron的src目录下
+    spawnSync("npm run", ["--prefix", webPath, "build"], { stdio: "inherit", shell: true });
+    fsExtra.moveSync(webDist, electronRenderder, { overwrite: true });
+    spawn("npm run", ["--prefix", electronPath, "build"], { stdio: "inherit", shell: true });
+}else {
+    spawnSync("npm run", ["--prefix", webPath, "build"], {
+      stdio: "inherit",
+      shell: true,
+    });
+    fsExtra.moveSync(webDist, rootDist, { overwrite: true });
+  }
