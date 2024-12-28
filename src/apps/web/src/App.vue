aac25a78088cb40e76417e3c64658cdfe8807e26
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-28 21:36:58 +0800

    fix

--- src/apps/web/src/App.vue
+++ src/apps/web/src/App.vue
@@ -2,14 +2,27 @@
   <router-view />
 </template>
 <script setup lang="ts">
-  console.log($electron);
+if ($electron) {
+  $electron.checkUpdate();
+  $electron.onUpdate((info) => {
+    // console.log('收到检测更新结果', info);
+
+    if (info.type === 'update-available') {
+      console.log('有可用更新，开始调用下载');
+      $electron.downloadUpdate();
+    }
+
+    if (info.type === 'download-progress') {
+      console.log('更新文件下载中：', Math.floor(info.data.percent)+'%');
+    }
+
+    if (info.type === 'update-downloaded') {
+      console.log('更新文件下载完成😊：', info.data);
+      console.log('5s之内 重启安装');
+      setTimeout(() => {
+        $electron.quitAndInstall();
+      }, 5000);
+    }
+  });
+}
 </script>
-
-
-<!-- if (window.$electron) {
-  // $electron.checkUpdate();
-  // $electron.onUpdate((info) => {
-  //   const eventName = 'on' + kebabCase_to_camelCase(info.type, true);
-  //   checkUpdateEvent[eventName](info.data);
-  // });
-} -->
