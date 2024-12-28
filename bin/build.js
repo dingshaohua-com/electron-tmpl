55fc2ea36ff9bbebba5e2ff7f65ed0979b478ed1
丁少华 <dingshaohua@douyuxingchen.com>
2024-12-28 16:25:53 +0800

    fix

---
+++ bin/build.js
@@ -0,0 +1,25 @@
+import fs from "node:fs";
+import path from "node:path";
+import { spawn, spawnSync } from "node:child_process";
+import fsExtra from "fs-extra/esm";
+
+const projectRoot = path.resolve(process.cwd());
+const rootDist = path.resolve(projectRoot, "dist");
+
+// 获取都有哪些项目
+const ignoreDir = [".DS_Store"];
+const appsDir = path.resolve("src", "apps");
+const apps = fs.readdirSync(appsDir, { withFileTypes: true }).filter(it=>!ignoreDir.includes(it.name));
+
+// 开始打包
+apps.forEach((app) => {
+  if (ignoreDir.includes(app.name)) return false;
+  const appPath = path.resolve(app.parentPath, app.name);
+  spawnSync("npm run", ["--prefix", appPath, "build"], {
+    stdio: "inherit",
+    shell: true,
+  });
+  const appDist = path.resolve(appPath, "dist");
+  const appDistNewPath = path.resolve(rootDist, app.name);
+  fsExtra.moveSync(appDist, appDistNewPath, { overwrite: true });
+});
